f27f0ff7b5ab57b74b7fe9c6b9358804bf7fd9fc
mmdjvdrp <mohammadjrahimpour@gmail.com>
2026-06-10 12:07:20 +0330

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,17 @@
+import {
+  auth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signOut,
+  onAuthStateChanged
+}
+from "./firebase.js";
+
+import {
+  load,
+  save
+}
+from "./storage.js";
 window.addEventListener(...)
 document.getElementById(...)
 onclick ها
