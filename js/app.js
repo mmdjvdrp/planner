5c62ab8329ed9a0dc198b81e4562e31637d73b13
mmdjvdrp <mohammadjrahimpour@gmail.com>
2026-06-10 17:55:06 +0330

    Create app.js

---
+++ js/app.js
@@ -0,0 +1,22 @@
+import { auth } from "./firebase.js";
+
+console.log("APP LOADED");
+console.log(auth);
+
+import {
+  auth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signOut,
+  onAuthStateChanged
+}
+from "./firebase.js";
+
+import {
+  load,
+  save
+}
+from "./storage.js";
+window.addEventListener(...)
+document.getElementById(...)
+onclick ها
