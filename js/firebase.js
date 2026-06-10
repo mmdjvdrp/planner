6ed0b4d2beca71496b5e7645618b90f4de436363
mmdjvdrp <mohammadjrahimpour@gmail.com>
2026-06-10 17:57:32 +0330

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,1 +1,38 @@
+import { initializeApp }
+from "https://www.gstatic.com/firebasejs/12.4.0/firebase-app.js";
 
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signOut,
+  onAuthStateChanged
+}
+from "https://www.gstatic.com/firebasejs/12.4.0/firebase-auth.js";
+
+import {
+  getDatabase,
+  ref,
+  get,
+  set
+}
+from "https://www.gstatic.com/firebasejs/12.4.0/firebase-database.js";
+
+const firebaseConfig = {
+  ...
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+export const db = getDatabase(app);
+
+export {
+  ref,
+  get,
+  set,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signOut,
+  onAuthStateChanged
+};
